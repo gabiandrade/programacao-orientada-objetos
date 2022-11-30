1f4ae24107ccaac0a4c045bca485409735586c29
gabiandrade <gabrielav.sudo@gmail.com>
2022-11-30 19:43:37 -0300

    commit de teste main

--- src/com/ada/example/Main.java
+++ src/com/ada/example/Main.java
@@ -3,5 +3,6 @@ package com.ada.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Programação Orientada a Objetos!");
+        System.out.println("Aula do dia 30/11");
     }
 }
